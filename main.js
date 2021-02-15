06d69499b6c15260272e80a31d7cb59e07942725
imadaqel <emadaqel92@gmail.com>
2021-02-15 21:25:03 +0200

    edit write func

--- main.js
+++ main.js
@@ -2,8 +2,6 @@
 var fs = require('fs');
 var parse = require('csv-parse');
 var inputFile = './MOCK_DATA.csv';
-console.log("Processing Countries file");
-
 var arr = []
 var count = 0;
 var readPlain = parse({ delimiter: ',' }, function (err, data) {
@@ -16,22 +14,32 @@ var readPlain = parse({ delimiter: ',' }, function (err, data) {
             , "gender": line[5]
             , "ip_address": line[6]
             , "color": line[7]
-            , "parentId": line[8]
+            , "parentId": line[0]
         };
         count++;
         if (count > 1) {
-            arr.push(JSON.stringify(country));
+            arr.push(country);
         }
         if (count == 200) {
-
             console.log(arr);
-            var str = arr.toString()
-            fs.writeFile('MOCK_DATA.json', str, (err) => {
-                if (err) throw err;
-                console.log('Lyric saved!');
-            });
+            var data = JSON.stringify(arr)
+            saveToFile(data)
         }
     });
 });
 let reader = fs.createReadStream(inputFile).pipe(readPlain);
 
+function saveToFile(data) {
+    fs.writeFile('MOCK_DATA.json', data, (err) => {
+        if (err) throw err;
+        console.log('Lyric saved!');
+    });
+}
+
+function readJsonFile() {
+    fs.readFile('./MOCK_DATA.json', 'utf8', function (err, data) {
+        console.log(data);
+    });
+    console.log('readFile called');
+}
+// readJsonFile()
